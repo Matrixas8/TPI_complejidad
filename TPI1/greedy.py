7acb532d75308fe3e4dd29fac914e5ffe70fbe46
Matrixas8 <mm3082426gmail.com>
2024-06-16 22:23:26 -0300

    ultimas modificaciones

--- TPI1/greedy.py
+++ TPI1/greedy.py
@@ -3,53 +3,43 @@ from math import ceil
 def organizar_estudio_voraz(n, duration, names, D):
     planner = {}
     tot_hours = 0
-    tot_hours = sum(duration) # Cantidad de horas qeu lleva estudiar todas las materias
-    avg_hours = ceil(tot_hours/D) # Cantidad de horas por día
+    for i in range(n): tot_hours += duration[i] 
+    avg_hours = ceil(tot_hours/D)
     
     i = 0 # aumenta los dias
-    #d = 0 # controla que no se pase de dias
-    #t = 0 # controla las materias que ya cumplieron con las horas necesarias
     m = 0 # controla que materia hay que estudiar
 
-    while i < D and any(duration):
+    while i<D and any(duration):
         if duration[m] >= avg_hours:
             planner[f'Dia {i+1}'] = {names[m]:avg_hours}
             duration[m] -= avg_hours
-            
             if (m+1)<n:
                 if duration[m+1] > duration[m]:
                     m += 1
             else:
-               m = 0
-            
+                m = 0
             i += 1
-            
         else:
             if duration[m] > 0:
                 planner[f'Dia {i+1}'] = {names[m]:duration[m]}
                 duration[m] = 0
                 i += 1
             else:
-                m = (m + 1) % n
-
-#        t = 0
-#        for j in range(n):
-#            if duration[j] == 0:
-#                t += 1
-        
+                m = (m+1)%n
     return planner  
 
 # Datos de prueba
-n = 6 # Cantidad de materias
-duration = [10, 8, 8, 7, 5, 3] # Duraciónen horas de cada materia
-names = ['Diseño','Base de datos','Complejidad','Economia','Comunicaciones','Planificacion'] #materias
-D = 10 # Cantidad de dias disponibles
+duration = [10, 8, 8, 7, 5, 3]
+names = ['Diseño','Base de datos','Complejidad','Economia','Comunicaciones','Planificacion']
+D = 10
 
-resultados = organizar_estudio_voraz(n,duration,names,D)
+resultados = organizar_estudio_voraz(len(names),duration,names,D)
 
-print('Planificacion de estudio:')
-for dia,materias in resultados.items():
-    print(f'{dia}:')
-    for materia,horas in materias.items():
-        print(f'    {materia}: {horas} horas')
-
+if any(duration):
+  print("No hay dias suficientes para alcanzar el objetivo")
+else:
+  print('Planificacion de estudio:')
+  for dia,materias in resultados.items():
+      print(f'{dia}:')
+      for materia,horas in materias.items():
+          print(f'    {materia}: {horas} horas')
